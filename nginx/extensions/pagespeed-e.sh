7431958be52e4660f362efa6da4f53b04ed5792b
Leonardo Noering <leonardo@noering.com.br>
2016-12-11 21:54:43 -0200

    fix '.'

--- nginx/extensions/pagespeed-e.sh
+++ nginx/extensions/pagespeed-e.sh
@@ -11,9 +11,9 @@
 BASEDIR=$(dirname "$0")
 NPS_VERSION=1.10.33.4
 
-wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -O .${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta.zip
-unzip -d .${BASEDIR%/*/*}/download/ .${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta.zip
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip -O ${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta.zip
+unzip -d ${BASEDIR%/*/*}/download/ ${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta.zip
 
 #wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -O psol-${NPS_VERSION}.tar.gz -P .${BASEDIR%/*/*}/download/
-wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -O .${BASEDIR%/*/*}/download/psol-${NPS_VERSION}.tar.gz
-tar xzvf .${BASEDIR%/*/*}/download/psol-${NPS_VERSION}.tar.gz -C .${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta/ # extracts to psol/
+wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz -O ${BASEDIR%/*/*}/download/psol-${NPS_VERSION}.tar.gz
+tar xzvf ${BASEDIR%/*/*}/download/psol-${NPS_VERSION}.tar.gz -C ${BASEDIR%/*/*}/download/ngx_pagespeed-release-${NPS_VERSION}-beta/ # extracts to psol/
